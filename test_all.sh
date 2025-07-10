5f121b1481d4e561a5f0b52d6bf3315a37fbabdf
Danny Caballero <danny.caballero@pm.me>
2025-07-10 13:08:14 -0400

    removed extra scripts

--- test_all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-python validate_yaml.py
-python validate_jb_toc.py
-python print_toc_tree.py
-sh test_jb_build.sh
--- test_jb_build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-jupyter-book build . --all --keep-going
-if [ $? -eq 0 ]; then
-  echo "[OK] Jupyter Book build succeeded."
-else
-  echo "[ERROR] Jupyter Book build failed."
-  exit 1
-fi
