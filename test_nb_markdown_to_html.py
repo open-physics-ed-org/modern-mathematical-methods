4f452151e652f7537c19b6e3b43e96a4dcbc74e9
Danny Caballero <danny.caballero@pm.me>
2025-07-10 02:21:39 -0400

    removed some scripts

--- test_nb_markdown_to_html.py
+++
@@ -1,6 +0,0 @@
-import nbformat, markdown
-nb = nbformat.read("content/notebooks/3_waves/notes-waves_intro.ipynb", as_version=4)
-for i, cell in enumerate(nb['cells']):
-    if cell['cell_type'] == 'markdown':
-        html = markdown.markdown(''.join(cell['source']))
-        print(f"Cell {i+1} HTML:\n{html}\n{'-'*40}")
--- test_nbformat_read.py
+++
@@ -1,5 +0,0 @@
-import nbformat
-nb = nbformat.read("content/notebooks/3_waves/notes-waves_intro.ipynb", as_version=4)
-print("Cells:", len(nb['cells']))
-for i, cell in enumerate(nb['cells']):
-    print(f"Cell {i+1}: type={cell['cell_type']}")
